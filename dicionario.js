af75a9f02aac2663d59ea3dd8b01892c78144482
MarianoMatteV <144505951+MarianoMatteV@users.noreply.github.com>
2023-12-06 12:38:02 -0300

    Atividade dicionário
    Precisa arrumar algumas coisas no código.,

---
+++ dicionario.js
@@ -0,0 +1,221 @@
+const fechar = document.querySelectorAll('.exit')
+const modal = document.querySelectorAll('.modal')
+const palavra = document.querySelectorAll('.palavra')
+
+palavra[0].addEventListener('click', () => {
+    modal[0].style.display = 'block'
+})
+
+fechar[0].addEventListener('click', () => {
+    modal[0].style.display = 'none'
+})
+
+function playAudio1(){
+    let x = document.getElementById("audio1");
+    x.load();
+    x.play();
+}
+
+function pauseAudio1(){
+    let x = document.getElementById("audio1");
+    x.pause();
+}
+
+
+
+palavra[1].addEventListener('click', () => {
+    modal[1].style.display = 'block'
+})
+
+fechar[1].addEventListener('click', () => {
+    modal[1].style.display = 'none'
+})
+
+function playAudio2(){
+    let x = document.getElementById("audio2");
+    x.load();
+    x.play();
+}
+
+function pauseAudio2(){
+    let x = document.getElementById("audio2");
+    x.pause();
+}
+
+
+
+palavra[2].addEventListener('click', () => {
+    modal[2].style.display = 'block'
+})
+
+fechar[2].addEventListener('click', () => {
+    modal[2].style.display = 'none'
+})
+
+function playAudio3(){
+    let x = document.getElementById("audio3");
+    x.load();
+    x.play();
+}
+
+function pauseAudio3(){
+    let x = document.getElementById("audio3");
+    x.pause();
+}
+
+
+
+palavra[3].addEventListener('click', () => {
+    modal[3].style.display = 'block'
+})
+
+fechar[3].addEventListener('click', () => {
+    modal[3].style.display = 'none'
+})
+
+function playAudio4(){
+    let x = document.getElementById("audio4");
+    x.load();
+    x.play();
+}
+
+function pauseAudio4(){
+    let x = document.getElementById("audio4");
+    x.pause();
+}
+
+
+
+palavra[4].addEventListener('click', () => {
+    modal[4].style.display = 'block'
+})
+
+fechar[4].addEventListener('click', () => {
+    modal[4].style.display = 'none'
+})
+
+function playAudio5(){
+    let x = document.getElementById("audio5");
+    x.load();
+    x.play();
+}
+
+function pauseAudio5(){
+    let x = document.getElementById("audio5");
+    x.pause();
+}
+
+
+palavra[5].addEventListener('click', () => {
+    modal[5].style.display = 'block'
+})
+
+fechar[5].addEventListener('click', () => {
+    modal[5].style.display = 'none'
+})
+
+function playAudio6(){
+    let x = document.getElementById("audio6");
+    x.load();
+    x.play();
+}
+
+function pauseAudio6(){
+    let x = document.getElementById("audio6");
+    x.pause();
+}
+
+
+palavra[6].addEventListener('click', () => {
+    modal[6].style.display = 'block'
+})
+
+fechar[6].addEventListener('click', () => {
+    modal[6].style.display = 'none'
+})
+
+function playAudio7(){
+    let x = document.getElementById("audio7");
+    x.load();
+    x.play();
+}
+
+function pauseAudio7(){
+    let x = document.getElementById("audio7");
+    x.pause();
+}
+
+
+
+palavra[7].addEventListener('click', () => {
+    modal[7].style.display = 'block'
+})
+
+fechar[7].addEventListener('click', () => {
+    modal[7].style.display = 'none'
+})
+
+function playAudio8(){
+    let x = document.getElementById("audio8");
+    x.load();
+    x.play();
+}
+
+function pauseAudio8(){
+    let x = document.getElementById("audio8");
+    x.pause();
+}
+
+
+
+palavra[8].addEventListener('click', () => {
+    modal[8].style.display = 'block'
+})
+
+fechar[8].addEventListener('click', () => {
+    modal[8].style.display = 'none'
+})
+
+function playAudio9(){
+    let x = document.getElementById("audio9");
+    x.load();
+    x.play();
+}
+
+function pauseAudio9(){
+    let x = document.getElementById("audio9");
+    x.pause();
+}
+
+
+
+palavra[9].addEventListener('click', () => {
+    modal[9].style.display = 'block'
+})
+
+fechar[9].addEventListener('click', () => {
+    modal[9].style.display = 'none'
+})
+
+function playAudio10(){
+    let x = document.getElementById("audio10");
+    x.load();
+    x.play();
+}
+
+function pauseAudio10(){
+    let x = document.getElementById("audio10");
+    x.pause();
+}
+
+function abrir_texto(){
+    let container = document.getElementById("container2")
+
+    if(container.style.display === "none")
+        container.style.display = "block"
+    else{
+        container.style.display = "none";
+    }
+}
+
+
